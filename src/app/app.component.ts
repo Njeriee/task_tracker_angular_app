a3569d457004b4ba6b746b9c57cafe1e9addfdd4
Derek Addo <mr.derek.addo@gmail.com>
2024-03-16 08:59:54 +0000

    button and header components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    // the selector is the html tag of your component that will be renderd in another component
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    // register your components here using their export name from their export class in the ts component
+    imports: [CommonModule, RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
-  title = 'angular-crash';
+  // declare component functionalities here i.e lifecycle methods
 }
